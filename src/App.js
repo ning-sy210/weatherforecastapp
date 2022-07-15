9389970e479f2766884c4d64bd2f678c56cb7205
Ning Sheng Ying <ning_sheng_ying@outlook.com>
2022-07-15 13:42:30 +0800

    Implement update of "date" and "time" state from DateTimeField component

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useReducer } from "react";
 import "./App.css";
 
 import DateTimeField from "./components/DateTimeField";
@@ -5,10 +6,39 @@ import LocationList from "./components/LocationList";
 import TrafficImageFetcher from "./components/TrafficImageFetcher";
 import WeatherDisplay from "./components/WeatherDisplay";
 
+export const ACTIONS = {
+  SET_DATE: "set-date",
+  SET_TIME: "set-time",
+  SET_LOCATION: "set-location",
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case ACTIONS.SET_DATE:
+      return { ...state, date: action.payload.date };
+    case ACTIONS.SET_TIME:
+      return { ...state, time: action.payload.time };
+    case ACTIONS.SET_LOCATION:
+      return { ...state, location: action.payload.location };
+    default:
+      return state;
+  }
+}
+
 function App() {
+  const [appState, dispatch] = useReducer(reducer, {
+    date: "",
+    time: "",
+    location: "",
+  });
+
   return (
     <div>
-      <DateTimeField />
+      <DateTimeField
+        date={appState["date"]}
+        time={appState["time"]}
+        dispatch={dispatch}
+      />
       <LocationList />
       <WeatherDisplay />
       <TrafficImageFetcher />
--- src/components/DateTimeField.js
+++ src/components/DateTimeField.js
@@ -1,32 +1,33 @@
 import React from "react";
-import { useState } from "react";
 
 import TextField from "@mui/material/TextField";
 
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import { AdapterMoment } from "@mui/x-date-pickers/AdapterMoment";
 import { DatePicker, TimePicker } from "@mui/x-date-pickers";
+import { ACTIONS } from "../App";
 
-const DateTimeField = () => {
-  var currDateTime = new Date();
-  const [selectedDate, setSelectedDate] = useState(currDateTime);
-  const [selectedTime, setSelectedTime] = useState(currDateTime);
-
+const DateTimeField = ({ date, time, dispatch }) => {
   return (
     <div>
       <LocalizationProvider dateAdapter={AdapterMoment}>
         <DatePicker
           label="Date"
-          inputFormat="DD/MM/yyyy"
-          value={selectedDate}
-          onChange={(date) => setSelectedDate(date)}
+          inputFormat="yyyy-MM-DD"
+          value={date}
+          onChange={(newDate) =>
+            dispatch({ type: ACTIONS.SET_DATE, payload: { date: newDate } })
+          }
           renderInput={(params) => <TextField {...params} />}
         />
         <TimePicker
           label="Time"
-          value={selectedTime}
-          onChange={(time) => setSelectedTime(time)}
+          value={time}
+          onChange={(newTime) =>
+            dispatch({ type: ACTIONS.SET_TIME, payload: { time: newTime } })
+          }
           renderInput={(params) => <TextField {...params} />}
+          closeOnSelect={false}
         />
       </LocalizationProvider>
     </div>
