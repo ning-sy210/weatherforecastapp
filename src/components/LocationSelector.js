f66cd259f6eab41a2872625f2e357dc4f66f1f1b
Ning Sheng Ying <ning_sheng_ying@outlook.com>
2022-07-15 17:49:12 +0800

    Fix: Change value of Select component to contain string

--- src/components/LocationSelector.js
+++ src/components/LocationSelector.js
@@ -24,7 +24,7 @@ const LocationSelector = ({ selectedLocation, locationList, dispatch }) => {
             {locationList.map((location) => (
               <MenuItem
                 key={`location-menuitem-${location["name"]}`}
-                value={location}
+                value={location["name"]}
               >
                 {location["name"]}
               </MenuItem>
